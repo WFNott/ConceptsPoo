594fc8a7885fd30a3aa3bd741633603f00c378b3
WFNott <walthersat@gmail.com>
2023-03-07 15:55:21 -0500

    Finish Date Class

---
+++ ConceptsPoo/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+/* se usa el using para agregar librerias o en este caso el proyecto, mismo donde esta alojado
+ nuestra clase DATE y otras mas que usaremos */
+
+using ConceptsPoo;
+Console.WriteLine("=============");
+Console.WriteLine("POO Concepts");
+Console.WriteLine("=============");
+
+// Se hace un try cash para en caso de falla enviar un mensaje al usuario
+try
+{
+    Console.WriteLine(new Date(2022, 2, 29));
+    Console.WriteLine(new Date(2025, 10, 11));
+	Console.WriteLine(new Date(2022, 11, 30));
+
+
+}
+
+/* Se usa el "Exception" el cual se heredo de "MonthException" y que contiene el mensaje
+   de error para el usuario, estos valores se pasan a la variable "error" y luego se imprime 
+   en pantalla*/
+
+catch (Exception error)
+{
+		Console.WriteLine(error.Message);
+}
+
